c5d5f3d9605f9a9ab316640387b474ec669a344a
unknown <=>
2024-05-03 20:07:33 -0300

    projeto java

---
+++ projeto-java/src/App.java
@@ -0,0 +1,9 @@
+import edu.joao.primeirasemana.Aula1;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        Aula1.execute();
+
+    }
+}
---
+++ projeto-java/src/edu/joao/primeirasemana/Aula1.java
@@ -0,0 +1,11 @@
+package edu.joao.primeirasemana;
+
+public class Aula1 {
+
+    public static void execute() {
+
+        System.out.println("testando um projeto java");
+     
+        
+    } 
+}
